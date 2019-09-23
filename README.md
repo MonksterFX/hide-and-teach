61ac281a7583cb282487b9b788511b297da881b5
Max Mönch <max.moench@hexfourty.com>
2019-09-23 15:21:40 +0200

    Changed from teaching cell to practice cell

--- README.md
+++ README.md
@@ -5,7 +5,18 @@ Jupyter Notebook Extension to provide exercise and solution in one document
 Possibilty to tag cells for export as exercise and solution and make the tagging visible (trough colorized borders)
 
 # How to use it
-tbd...
+1. install the extension
+2. activate it
+3. mark cells which you want to clean/drop
+4. use script to build clean version without practice cells
+
+# Goals and Improvements until version 1.0
+- add better instructions and description
+- add multiselect functionality (mark more than one cell)
+- make python file for convertion
+- make it more stylish
+- evaluate inclution of export function in extension
+
 
 
 
--- src/main.js
+++ src/main.js
@@ -3,73 +3,104 @@
 define([
     'base/js/namespace',
     'base/js/events'
-    ], function(Jupyter, events) {
-	
-	var make_teaching_cell = function(){
-		var selected_cells = Jupyter.notebook.get_selected_cells();
-		selected_cells[0].metadata["teaching_cell"] = true;
-		selected_cells[0].element.css("border", "2px dashed purple")
-	}
+], function (Jupyter, events) {
 
-	var clear_teaching_cell = function(){
-		var selected_cells = Jupyter.notebook.get_selected_cells();
-		selected_cells[0].metadata["teaching_cell"] = false;
-		selected_cells[0].element.css("border", "")
-	}
+    var make_practice_cell = function () {
+        var selected_cells = Jupyter.notebook.get_selected_cells();
+        selected_cells[0].metadata["practice_cell"] = true;
+        selected_cells[0].element.css("border", "2px dashed purple")
+    }
 
-	
-	var init_show_marker = function(){
-       		var cells = Jupyter.notebook.get_cells();
-        	for (var ii = 0; ii < cells.length; ii++) {
-            		var cell = cells[ii];
-            		if(cell.metadata["teaching_cell"] === true) {
-                		cell.element.css("border", "2px dashed purple");
-            		}
-		}
-	}
+    var clear_practice_cell = function () {
+        var selected_cells = Jupyter.notebook.get_selected_cells();
+        selected_cells[0].metadata["practice_cell"] = false;
+        selected_cells[0].element.css("border", "")
+    }
 
-      	var defaultCellButton = function () {
+    var show_marker = function(){
+        var cells = Jupyter.notebook.get_cells();
+        for (var ii = 0; ii < cells.length; ii++) {
+            var cell = cells[ii];
+            if (cell.metadata["practice_cell"] === true) {
+                cell.element.css("border", "2px dashed purple");
+            }
+        }
+    }
+    
+    var hide_marker = function(){
+        var cells = Jupyter.notebook.get_cells();
+        for (var ii = 0; ii < cells.length; ii++) {
+            var cell = cells[ii];
+            if (cell.metadata["practice_cell"] === true) {
+                cell.element.css("border", "None");
+            }
+        }
+    }
 
-	// https://jupyter-notebook.readthedocs.io/en/stable/extending/frontend_extensions.html#defining-and-registering-your-own-actions
-          Jupyter.toolbar.add_buttons_group([
-              Jupyter.keyboard_manager.actions.register ({
-                  'help': 'Tag as teaching cell',
-                  'icon' : 'fa-lock',
-                  'handler': make_teaching_cell 
-              }, 'make-teaching-cell', 'Make teaching cell'), 
 
-              Jupyter.keyboard_manager.actions.register ({
-                  'help': 'Untag as teaching cell',
-                  'icon' : 'fa-unlock-alt',
-                  'handler': clear_teaching_cell 
-              }, 'clean-teaching-cell', 'Clean teaching cell')
+    var init_show_marker = function () {
+        show_marker();
+    }
 
-          ])
-      }
+    // add buttons
+    var defaultCellButton = function () {
+
+        // https://jupyter-notebook.readthedocs.io/en/stable/extending/frontend_extensions.html#defining-and-registering-your-own-actions
+        Jupyter.toolbar.add_buttons_group([
+            Jupyter.keyboard_manager.actions.register({
+                'help': 'Tag as practice cell',
+                'icon': 'fa-minus-square',
+                'handler': make_practice_cell
+            }, 'make-practice-cell', 'Make practice cell'),
+
+            Jupyter.keyboard_manager.actions.register({
+                'help': 'Untag as practice cell',
+                'icon': 'fa-undo',
+                'handler': clear_practice_cell
+            }, 'clean-practice-cell', 'Clean practice cell'),
+        ])
+            
+
+            //  TODO: Clean cells and only output
+            // fa-eraser 
+        Jupyter.toolbar.add_buttons_group([
+            Jupyter.keyboard_manager.actions.register({
+                'help': 'Show all Marker',
+                'icon': 'fa-eye',
+                'handler': show_marker
+            },  'show-markers', 'Show Marker'),
+
+            Jupyter.keyboard_manager.actions.register({
+                'help': 'Hide all Marker',
+                'icon': 'fa-eye-slash',
+                'handler': hide_marker
+            },  'hide-markers', 'Hide Marker'),
+        ])
+    }
 
     function load_ipython_extension() {
-         "use strict";
+        "use strict";
 
-       	defaultCellButton();
+        defaultCellButton();
 
-        if (Jupyter.notebook._fully_loaded) {  
-		init_show_marker();
+        if (Jupyter.notebook._fully_loaded) {
+            init_show_marker();
         } else {
-            $([Jupyter.events]).on("notebook_loaded.Notebook", function() {
-		init_show_marker();
+            $([Jupyter.events]).on("notebook_loaded.Notebook", function () {
+                init_show_marker();
             })
         }
-}
-/*
-    function run_init_cells_asap () {
-        if (Jupyter.notebook && Jupyter.notebook.kernel && Jupyter.notebook.kernel.info_reply.status === 'ok') {
-            // kernel is already ready
-            init_show_marker();
-        }
-        // whenever a (new) kernel  becomes ready, run all initialization cells
-        events.on('kernel_ready.Kernel', run_init_cells);
-}
-*/
+    }
+    /*
+        function run_init_cells_asap () {
+            if (Jupyter.notebook && Jupyter.notebook.kernel && Jupyter.notebook.kernel.info_reply.status === 'ok') {
+                // kernel is already ready
+                init_show_marker();
+            }
+            // whenever a (new) kernel  becomes ready, run all initialization cells
+            events.on('kernel_ready.Kernel', run_init_cells);
+    }
+    */
 
     return {
         load_ipython_extension: load_ipython_extension
